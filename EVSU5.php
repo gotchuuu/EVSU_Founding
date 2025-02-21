d85e23f0bb42c5531febca56d66f50f1f441c167
John Godwin <155992531+gotchuuu@users.noreply.github.com>
2025-02-21 13:42:08 +0800

    Add files via upload

---
+++ EVSU5.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        .custom-title {
+            font-family: 'Josefin Sans', sans-serif;
+            color: white;
+            font-weight: bold;
+        }
+    </style>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Creative - Start Bootstrap Theme</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+               
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="EVSU1.php">DAY 1</a></li>
+                        <li class="nav-item"><a class="nav-link" href="EVSU2.php">DAY 2</a></li>
+                        <li class="nav-item"><a class="nav-link" href="EVSU3.php">DAY 3</a></li>
+                        <li class="nav-item"><a class="nav-link" href="EVSU4.php">DAY 4</a></li>
+                        <li class="nav-item"><a class="nav-link" href="EVSU5.php">DAY 5</a></li>
+                        
+                        <li class="nav-item"><a class="nav-link" href="index.php">Back</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </nav>
+
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                    <h1 class="custom-title">Eastern Visayas State University 118th Founding Anniversary</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">One EVSU: Building Futures through Smart Innovations.</p>
+                        <h1 class="text-white-75 mb-5" >DAY 5</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <div id="portfolio">
+        <div class="container-fluid p-0">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-0">
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/day7.jpg" title="Day 1">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/day7.jpg" alt="Day 1" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/day9.jpg" title="Day 2">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/day9.jpg" alt="Day 2" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/day5.jpg" title="Day 3">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/day5.jpg" alt="Day 3" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/day10.jpg" title="Day 4">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/day10.jpg" alt="Day 4" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/day8.jpg" title="Day 5">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/day8.jpg" alt="Day 5" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a class="portfolio-box" href="assets/img/portfolio/fullsize/Day7.jpg" title="Day 6">
+                    <img class="img-fluid" src="assets/img/portfolio/thumbnails/Day7.jpg" alt="Day 6" />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">DAY 5</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+        <!-- Call to action-->
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
